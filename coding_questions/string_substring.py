5aa888be1e633618df3d85de27049fee2c1f2831
Omar Shaikh <senseofomar@gmail.com>
2026-03-10 19:29:38 +0530

    new logic based approach, no slicing

--- coding_questions/string_substring.py
+++ coding_questions/string_substring.py
@@ -1,3 +1,29 @@
+# best interview approach, fully logic based no slicing
+def substring_finder(s, subs):
+
+    n = len(s)
+    m = len(subs)
+
+    for i in range(n - m + 1):
+
+        match = True
+
+        for j in range(m):
+
+            if s[i + j] != subs[j]:
+                match = False
+                break
+
+        if match:
+            return True
+
+    return False
+
+
+print(substring_finder("hello world", "hello"))
+
+
+# second best approach using slicing
 def contains_substring(string, substring):
 
     n = len(string)
@@ -16,6 +42,8 @@ string = "Hello World"
 print(contains_substring(string, "Hello"))
 print(contains_substring(string, "World"))
 
+
+# pythonic way
 def contains_substring1(string, substring):
     return substring in string
 
@@ -24,3 +52,4 @@ string = "Hello World"
 
 print(contains_substring(string, "Hello"))
 print(contains_substring(string, "World"))
+
