e21407456ff473b6a609b9c42f3df06bd9ec0da5
Omar Shaikh <senseofomar@gmail.com>
2026-04-06 01:28:22 +0530

    pythonic way

--- coding_questions/even_odd_arrangement.py
+++ coding_questions/even_odd_arrangement.py
@@ -10,14 +10,4 @@ def even_odd_arrangement(num:int)->int:
             odds.append(n)
     return int("".join(evens) + "".join(odds))
 
-def rearrange_digits(s: str) -> str:
-    even_pos = []
-    odd_pos = []
 
-    for i in range(len(s)):
-        if i % 2 == 0:   # even index
-            even_pos.append(s[i])
-        else:            # odd index
-            odd_pos.append(s[i])
-
-    return "".join(even_pos) + "".join(odd_pos)
