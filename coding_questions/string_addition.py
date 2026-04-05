5503b0c77c7fe80cbe43a1763a85d9ce8f9411cc
Omar Shaikh <senseofomar@gmail.com>
2026-04-05 23:51:16 +0530

    pythonic way

--- coding_questions/string_addition.py
+++ coding_questions/string_addition.py
@@ -1,7 +1,1 @@
-def string_addition(s:str, add_string:str)->str:
-    return add_string + s
 
-print(string_addition("what do you want", "tell me "))
-
-def add_do_not(s: str) -> str:
-    return "do not " + s
