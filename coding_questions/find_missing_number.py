d026062a8f9d905e3d4f5f548693cb260898a4d3
Omar Shaikh <senseofomar@gmail.com>
2026-04-07 15:36:55 +0530

    pythonic way

--- coding_questions/find_missing_number.py
+++ coding_questions/find_missing_number.py
@@ -11,23 +11,3 @@ N = 5
 
 print(find_missing_sum(arr, N))
 
-def find_missing_xor(arr, n):
-
-    xor_all = 0
-    xor_arr = 0
-
-    for i in range(1, N+1):
-        xor_all ^= i
-
-    for num in arr:
-        xor_arr ^= num
-
-    missing = xor_all ^ xor_arr
-
-    return missing
-
-
-array = [1,2,4,5]
-N = 5
-
-print(find_missing_xor(array, N))
