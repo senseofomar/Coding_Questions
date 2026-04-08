0d168612810ae786d166c19f9326acca2109d9b6
Omar Shaikh <senseofomar@gmail.com>
2026-04-09 00:10:17 +0530

    pythonic way

--- coding_questions/string_to_decimal.py
+++ coding_questions/string_to_decimal.py
@@ -1,4 +1,17 @@
+def string_to_decimal(Str:str)->int:
+    vowels = "aeiou"
+    binary_str = ""
 
+    for ch in Str:
+        if ch in vowels:
+            binary_str += "0"
+        else:
+            binary_str += "1"
+
+    decimal_num = int(binary_str, 2)
+    return decimal_num
+
+print(string_to_decimal("zoo"))
 
 
 
