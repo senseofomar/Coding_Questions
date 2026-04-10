4518d5fe2e3a651444b1caca6e05db238c0784ef
Omar Shaikh <senseofomar@gmail.com>
2026-04-10 23:57:17 +0530

    pythonic way

--- coding_questions/string_substring.py
+++ coding_questions/string_substring.py
@@ -1,4 +1,26 @@
+# best interview approach, fully logic based no slicing
+def substring_finder(s, subs):
 
+    n = len(s)
+    m = len(subs)
+
+    for i in range(n - m + 1):
+
+        match = True  #Assume it matches unless proven otherwise
+
+        for j in range(m):
+
+            if s[i + j] != subs[j]:
+                match = False
+                break
+
+        if match:
+            return True
+
+    return False
+
+
+print(substring_finder("hello world", "hello"))
 
 
 # second best approach using slicing
