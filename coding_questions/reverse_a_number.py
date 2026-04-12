aba3f77e6b0782c45d9a5fefd94622c4a006d053
Omar Shaikh <senseofomar@gmail.com>
2026-04-13 00:17:53 +0530

    pythonic way

--- coding_questions/reverse_a_number.py
+++ coding_questions/reverse_a_number.py
@@ -11,4 +11,14 @@ def reverse_number(n:int)->str:
 
 print(reverse_number(12234))
 
+def rvn(n):
+    reversen = 0
+    while n>0:
+        rrem = n%10
+        reversen = reversen*10 + rrem
 
+        n = n//10
+
+    return reversen
+
+print(rvn(12423))
