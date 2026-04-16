746cdded76df2c50d212698235ed69bf4f968af4
Omar Shaikh <senseofomar@gmail.com>
2026-04-16 19:05:41 +0530

    pythonic way

--- coding_questions/GCD.py
+++ coding_questions/GCD.py
@@ -1,12 +1,1 @@
-def gcd_function(num1, num2):
-    if num1 > num2:
-        small = num2
-    else:
-        small = num1
-    for i in range(1, small +1):
-        if (num1 % i == 0) and (num2 % i ==0):
-            gcd = i
-    print("GCD of two Number: {}".format(gcd))
 
-
-gcd_function(44, 50)
